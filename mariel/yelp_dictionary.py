aeeb184689b7537b76ace561d15ea64e5220d940
Ben Hillmann <bhillmann@outlook.com>
2015-04-02 12:18:41 -0500

    Preprocessing documents almost done

--- mariel/yelp_dictionary.py
+++ mariel/yelp_dictionary.py
@@ -1,3 +1,33 @@
-from gensim import corpora, models, similarities
+from gensim import corpora
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from read_subjectivity_clues import Codeword
 
-dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
+def build_dictionary():
+    dictionary = corpora.Dictionary(preprocess_line() for line in open('mycorpus.txt'))
+    once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+    dictionary.filter_tokens(once_ids)
+    dictionary.compactify()
+    return dictionary
+
+def strip_line(line):
+    sentence = line.lower()
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(sentence)
+    filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
+    return " ".join(filtered_words)
+
+def stem(tokens):
+    wnl = WordNetLemmatizer()
+    return [wnl.lemmatize(token) for token in tokens]
+
+def tokenize(line):
+    return nltk.word_tokenize(line)
+
+def preprocess_line(line):
+    line = strip_line(line)
+    tokens = tokenize(line)
+    tokens = add_codeword_tokens(line)
+    return stem(tokens)
