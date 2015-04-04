7ca7c2aa0ee225d43e40408a9f8f95e7f39354a1
Ben Hillmann <bhillmann@outlook.com>
2015-04-04 16:07:44 -0500

    Multicore wasn't working sooo.

--- mariel/yelp_lda.py
+++ mariel/yelp_lda.py
@@ -1,10 +1,13 @@
-from gensim.models.ldamulticore import LdaMulticore
+from gensim import models
 from gensim import corpora
 
-corpora_path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/real.dict'
-d = corpora.Dictionary.load(corpora_path)
+if __name__ == "__main__":
+    corpora_path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/real.dict'
+    d = corpora.Dictionary.load(corpora_path)
 
-corpus_path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/corpus.lda-c'
-corpus = corpora.BleiCorpus.load(corpus_path)
+    corpus_path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/corpus.mm'
+    corpus = corpora.MmCorpus(corpus_path)
 
-print(corpus)
+    lda = models.LdaModel(corpus, id2word=d, num_topics=20, passes=20)
+
+    print(corpus)
