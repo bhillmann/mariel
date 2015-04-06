feafb55e148db77e27e13de0b1d6cdea0879bd3e
Ben Hillmann <bhillmann@outlook.com>
2015-04-05 20:34:23 -0500

    Printing updates in midwest

--- mariel/yelp_stream.py
+++ mariel/yelp_stream.py
@@ -7,10 +7,14 @@ def yield_midwest_json():
     ids = get_midwest_ids()
     path = os.path.join(os.path.dirname(__file__),
                         '../data/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json')
+    s = 0
     with codecs.open(path, encoding='utf8') as f:
         for line in f:
             json = ujson.loads(line)
-            if json['business'] in ids:
+            if json['business_id'] in ids:
+                s += 1
+                if s % 1000 == 0:
+                    print(s)
                 yield json
 
 def get_midwest_ids():
