a2f9a391cd68145dbaba5aa2076b3a9e4e5b3a81
Ben Hillmann <bhillmann@outlook.com>
2015-04-08 18:33:45 -0500

    Fixed the annotations directories.

--- mariel/yelp_annotations.py
+++ mariel/yelp_annotations.py
@@ -1,12 +1,13 @@
 from yelp_stream import yield_midwest_json
 from pandas import Series
+import os
 
 
 def get_star_vector():
     return Series([d['stars'] for d in yield_midwest_json()])
 
-
 if __name__ == "__main__":
     xx = get_star_vector()
-    path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/star_vector.csv'
+    path = os.path.join(os.path.dirname(__file__), 'cache')
+    path = os.path.join(path, 'star_vector.csv')
     xx.to_csv(path)
