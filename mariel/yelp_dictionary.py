e645367a058683fbc402dcc7fcde4c0f44851766
Ben Hillmann <bhillmann@outlook.com>
2015-04-08 01:09:02 -0500

    Updating for final stream

--- mariel/yelp_dictionary.py
+++ mariel/yelp_dictionary.py
@@ -1,13 +1,19 @@
 from yelp_stream import yield_midwest_json
 from kaggle_preprocessing import preprocess_pipeline
 from gensim import corpora
+import os
 
 if __name__ == '__main__':
     d_final = corpora.Dictionary(
         preprocess_pipeline(d['text'], "english", "LancasterStemmer", False, True, True) for d in
         yield_midwest_json()
     )
-    once_ids = [tokenid for tokenid, docfreq in d_final.dfs.items() if docfreq == 1]
+    once_ids = [tokenid for tokenid, docfreq in d_final.dfs.items() if docfreq <= 4]
     d_final.filter_tokens(once_ids)
     d_final.compactify()
-    d_final.save('cache/yelp_blei.dict')
+    path = os.path.join(os.path.dirname(__file__), 'cache')
+    d_final.save(path + 'yelp_blei.dict')
+    with open(path+'token2id.dat', 'w') as f:
+        for key in d_final.keys():
+            f.write(key + '/n')
+
--- mariel/yelp_reviews.py
+++ mariel/yelp_reviews.py
@@ -1,1 +1,12 @@
-__author__ = 'Benjamin'
+from yelp_stream import yield_midwest_json
+from pandas import Series
+
+
+def get_star_vector():
+    return Series([d['stars'] for d in yield_midwest_json()])
+
+
+if __name__ == "__main__":
+    xx = get_star_vector()
+    path = 'C:/Users/Benjamin/Projects/mariel/mariel/cache/star_vector.csv'
+    xx.to_csv(path)
